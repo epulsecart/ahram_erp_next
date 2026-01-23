47e42702525b5a40b636a12662e9f1ec7d43e996
epulsecart <epulsecompany@gmail.com>
2026-01-23 20:02:51 +0000

    Add EPC App Settings doctype

---
+++ epc_app/epc_app/doctype/epc_app_settings/epc_app_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, yahya basalama and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("EPC App Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
