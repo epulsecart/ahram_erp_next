17274946ebe9fb2e56754a53f2ddd9633eaf8252
epulsecart@gmail.com <epulsecart@gmail.com>
2026-01-25 11:55:12 +0000

    make the commission child slap table non custom

---
+++ epc_app/epc_app/doctype/epc_commission_slab/epc_commission_slab.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, yahya basalama and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EPCCommissionSlab(Document):
+	pass
